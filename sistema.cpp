4f76171e3e41946ef249c39922e85190cdfa5cfa
Daniel Vítor <danielvitoroliveirab@gmail.com>
2025-06-09 17:09:18 -0300

    projeto final

--- sistema.cpp
+++ sistema.cpp
@@ -8,6 +8,9 @@ using namespace std;
 int main() {
     // Vetor de ponteiros para 10 funcionários
     Funcionario *vetor[10];
+    int id, horas, projetos;
+    float salario, bonus;
+    string nome;
 
     // Perguntando ao usuário quantos funcionários ele quer cadastrar
     int n;
@@ -20,26 +23,81 @@ int main() {
     }
     
     char tipo;
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++){ // Cadastro dos funcionários
+        cout << endl;
         cout << "Que tipo de funcionário você quer cadastrar? (digite D para desenvolvedor, E para estagiário e G para gerente)" << endl;
         cin >> tipo;
         while (tipo != 'D' && tipo != 'E' && tipo != 'G'){
+            cout << endl;
             cout << "Digite um tipo de funcionário válido (D = desenvolvedor, E = estagiário, G = gerente)" << endl;
             cin >> tipo;
         }
         switch (tipo) {
             case 'D':
                 vetor[i] = new Desenvolvedor;
+
+                cout << "ID: ";
+                cin >> id;
+                vetor[i]->setId(id);
+
+                cout << "Nome: ";
+                cin >> nome;
+                vetor[i]->setNome(nome);
+
+                cout << "Quantidade de projetos: ";
+                cin >> projetos;
+                dynamic_cast<Desenvolvedor*>(vetor[i])->setQuantidadeDeProjetos(projetos);
+
+                cout << "Salário base: ";
+                cin >> salario;
+                vetor[i]->setSalarioBase(salario);
             break; 
 
             case 'E':
                 vetor[i] = new Estagiario;
+
+                cout << "ID: ";
+                cin >> id;
+                vetor[i]->setId(id);
+
+                cout << "Nome: ";
+                cin >> nome;
+                vetor[i]->setNome(nome);
+
+                cout << "Horas trabalhadas: ";
+                cin >> horas;
+                dynamic_cast<Estagiario*>(vetor[i])->setHorasTrabalhadas(horas); 
+
+                cout << "Salário base: ";
+                cin >> salario;
+                vetor[i]->setSalarioBase(salario);
             break;
 
             case 'G':
                 vetor[i] = new Gerente;
+
+                cout << "ID: ";
+                cin >> id;
+                vetor[i]->setId(id);
+
+                cout << "Nome: ";
+                cin >> nome;
+                vetor[i]->setNome(nome);
+
+                cout << "Bônus mensal: ";
+                cin >> bonus;
+                dynamic_cast<Gerente*>(vetor[i])->setBonusMensal(bonus);
+
+                cout << "Salário base: ";
+                cin >> salario;
+                vetor[i]->setSalarioBase(salario);
             break;
         }
+    }
+
+    for(int j = 0; j < n; j++){
+        cout << endl;
+        vetor[j]->exibirInformacoes();
     }
 
     return 0;
